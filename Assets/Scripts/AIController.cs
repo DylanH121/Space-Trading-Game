711ac5f1bd0fcc7d649a5af9edfcf02ed1269639
DylanH121 <83083970+DylanH121@users.noreply.github.com>
2025-11-17 21:11:28 -0500

    AI changes

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class AIBehaviour : MonoBehaviour {
+public class AIController : MonoBehaviour {
     // Movement
     [SerializeField] private float m_thrusterForce;
     [SerializeField] private float m_rotationForce;
@@ -16,11 +16,16 @@ public class AIBehaviour : MonoBehaviour {
 
     private bool m_hostileToPlayer = false;
 
-    private Transform m_moveTarget;
+    private Transform m_moveTarget = null;
     private Vector2 m_moveDirectionVector;
+    private bool m_isAlignedWithMoveTarget = false;
+
+    void Awake() {
+        m_rb = GetComponent<Rigidbody2D>();
+    }
 
     void Start() {
-        
+        ChooseRandomDirection();
     }
 
     private void ClampMaxSpeed() {
@@ -38,9 +43,10 @@ public class AIBehaviour : MonoBehaviour {
         Vector2 direction = new Vector2(Mathf.Cos(directionRadians), Mathf.Sin(directionRadians));
 
         m_moveDirectionVector = direction;
+        Debug.Log(m_moveDirectionVector);
     }
 
-    // Get angle difference between current forward direction and target vector
+    // Get angle difference in degrees between current forward direction and target vector
     private float GetAngleDifference(Vector2 vectorToTarget) {
         return Mathf.DeltaAngle(transform.eulerAngles.z, Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - 90f);
     }
@@ -50,21 +56,30 @@ public class AIBehaviour : MonoBehaviour {
 
         float turnForce = Mathf.Clamp(angleDifference * m_rotationForce, -m_maxTurnSpeed, m_maxTurnSpeed);
         m_rb.AddTorque(turnForce);
+
+        if (angleDifference < 0.01) {
+            m_isAlignedWithMoveTarget = true;
+        }
     }
 
     private void MoveAlongVector(Vector2 moveVector) {
         RotateToTargetVector(moveVector);
-        m_rb.AddForce(transform.up * m_thrusterForce);
+        if (m_isAlignedWithMoveTarget) {
+            m_rb.AddForce(transform.up * m_thrusterForce);
+        }
     }
 
     // Move away from centre of scene and jump/despawn
     private void MoveToExit() {
         MoveAlongVector(m_moveDirectionVector);
 
-
+        if (Vector2.Distance(transform.position, Vector2.zero) >= 25.0f) {
+            Destroy(this.gameObject);
+        }
     }
 
     void FixedUpdate() {
-        
+        MoveToExit();
+        ClampMaxSpeed();
     }
 }
---
+++ Assets/Scripts/MarketEntry.cs
@@ -0,0 +1,5 @@
+[System.Serializable]
+public struct MarketEntry {
+    public Commodity commodity;
+    public float priceMultiplier;
+}
